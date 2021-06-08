b0b837d4948a5653f67a837a3405061c54bba026
Jicheol Oh <jicheol@gmail.com>
2021-06-08 14:59:01 -0700

    Access SQL Server Database In .NET Core Console Application

---
+++ SqlClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using SqlClient.DataAccessLayer;
+using System;
+using System.IO;
+
+namespace SqlClient
+{
+    class Program
+    {
+        private static IConfiguration _configuration;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            GetAppSettingsFile();
+            PrintCountries();
+        }
+
+        private static void PrintCountries()
+        {
+            var countryDAL = new CountryDAL(_configuration);
+            var listCountryModel = countryDAL.GetList();
+
+            listCountryModel.ForEach(item =>
+            {
+                Console.WriteLine(item.Country);
+            });
+
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+
+        private static void GetAppSettingsFile()
+        {
+
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _configuration = builder.Build();
+            //Console.WriteLine(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
